71f73a435b76ac6fe830809d4e161f3ecaa06e45
Evgeny Novikov <evgeny.novikov@cedargame.com>
2021-02-11 16:38:27 +0300

    Добавил скрипт вращения бревна

---
+++ Assets/Scripts/CoreMechanics/LogRotation.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogRotation : MonoBehaviour
+{
+    [SerializeField] private AnimationCurve rotationCurve;
+    [SerializeField] private WrapMode curveMode;
+
+    private float currentTime;
+    private Coroutine rotationCoroutine;
+
+    private void Start()
+    {
+        rotationCurve.postWrapMode = curveMode;
+
+        StartCoroutine(StartRotation());
+    }
+
+    public void SetCurve(AnimationCurve rotationCurve, WrapMode curveMode)
+    {
+        this.rotationCurve = rotationCurve;
+        this.curveMode = curveMode;
+
+        rotationCoroutine = StartCoroutine(StartRotation());
+    }
+
+    public void StopRotation()
+    {
+        StopCoroutine(rotationCoroutine);
+        rotationCoroutine = null;
+    }
+
+    private IEnumerator StartRotation()
+    {
+        while (true)
+        {
+            transform.rotation = Quaternion.AngleAxis(rotationCurve.Evaluate(currentTime), Vector3.forward);
+            currentTime += Time.deltaTime;
+
+            yield return null;
+        }
+    }
+}
